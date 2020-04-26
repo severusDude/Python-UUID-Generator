35927b4d2e199af63e23a5ff04e465f9434e6ca0
severusDude <60343449+severusDude@users.noreply.github.com>
2020-04-26 20:15:22 +0700

    Added User Interface

---
+++ UUIDGenerator.py
@@ -0,0 +1,28 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import pyperclip
+
+
+class UserInterface(QtWidgets.QMainWindow):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(400, 200)
+        MainWindow.setWindowTitle("UUID Generator")
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+
+        # GENERATE UUID BUTTON
+        self.generate = QtWidgets.QPushButton(self.centralWidget)
+        self.generate.setGeometry(QtCore.QRect(160, 70, 75, 40))
+
+        # COPY OUTPUT BUTTON
+        self.copy_output = QtWidgets.QPushButton(self.centralWidget)
+        self.copy_output.setGeometry(QtCore.QRect(293, 120, 35, 35))
+
+        # OUTPUT
+        self.uuid_output = QtWidgets.QLabel(self.centralWidget)
+        self.uuid_output.setGeometry(QtCore.QRect(60, 120, 225, 35))
+        self.uuid_output.setFrameShape(QtWidgets.QFrame.Box)
+        self.uuid_output.setFrameShadow(QtWidgets.QFrame.Plain)
+
+        # CENTRAL WIDGET
+        MainWindow.setCentralWidget(self.centralWidget)
