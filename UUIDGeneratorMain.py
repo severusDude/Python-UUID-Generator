90227422f7bba98ae47cf640e31e445eae635143
severusDude <60343449+severusDude@users.noreply.github.com>
2020-04-26 20:15:44 +0700

    Added Main Control

---
+++ UUIDGeneratorMain.py
@@ -0,0 +1,32 @@
+# Backend for UUIDGenerator
+
+import pyperclip
+import sys
+from UUIDGenerator import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class ControlMainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.main_ui = UserInterface()
+        self.main_ui.setupUi(self)
+
+    # defining system exception
+    def log_uncaught_exceptions(self, ex_cls, ex, tb):
+        text = '{}: {}:\n'.format(ex_cls.__name__, ex)
+        import traceback
+        text += ''.join(traceback.format_tb(tb))
+
+        print(text)
+        self.main_ui.QMessageBox.critical(None, 'Error', text)
+        quit()
+
+    sys.excepthook = log_uncaught_exceptions
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    program = ControlMainWindow()
+    program.show()
+    sys.exit(app.exec_())
